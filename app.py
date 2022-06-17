15a20ff868e14fec2898bcbb06707366a6483303
Pit Kachanov <petrusha_kachanov_1999@mail.ru>
2022-06-17 06:00:28 +0300

    really last commit

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request
 import numpy
 import joblib
-MODEL_PATH = 'ml_models/model.pkl'
+
+RF_MODEL_PATH = 'ml_models/random_forest_model.pkl'
+CB_MODEL_PATH = 'ml_models/catboost_model.pkl'
 
 SCALER_X_PATH = 'ml_models/scaler_x.pkl'
 
@@ -12,28 +14,46 @@ app = Flask(__name__)
 @app.route('/predict_price', methods=['GET'])
 def predict():
     args = request.args
+    model = args.get('model', default=-1, type=int)
     open_plan = args.get('open_plan', default = -1, type = int)
     rooms = args.get('rooms', default = -1, type = int)
     area = args.get('area', default = -1, type = float)
     kitchen_area = args.get('kitchen_area', default = -1, type = float)
     living_area = args.get('living_area', default = -1, type = float)
 
-    #response = 'open_plan,:{}, rooms:{}, area:{}, kitchen_area:{}, living_area:{}'.format(open_plan, rooms, area, kithen_area, living_area)
-    model = joblib.load(MODEL_PATH)
-    sc_x = joblib.load(SCALER_X_PATH)
-    sc_y = joblib.load(SCALER_Y_PATH)
 
-    x = numpy.array([open_plan, rooms, area, kitchen_area, living_area]).reshape(1,-1)
+    if model == 1:
 
-    x = sc_x.transform(x)
+        # response = 'open_plan:{}, rooms:{}, area:{}, renovation:{}'.format(open_plan, rooms, area, renovation)
+        model = joblib.load(RF_MODEL_PATH)
+        sc_x = joblib.load(SCALER_X_PATH)
+        sc_y = joblib.load(SCALER_Y_PATH)
 
-    result = model.predict(x)
+        x = numpy.array([open_plan, rooms, area, kitchen_area, living_area]).reshape(1,-1)
+        x = sc_x.transform(x)
+        result = model.predict(x)
+        result = sc_y.inverse_transform(result.reshape(1, -1))
 
-    result = sc_y.inverse_transform(result.reshape(1,-1))
+        return str(result[0][0])
 
+    if model == 2:
 
+        # response = 'floor:{}, open_plan:{}, rooms:{}, area:{}.format(floor, open_plan, rooms, area, renovation)
 
-    return str(result[0][0])
+        model = joblib.load(CB_MODEL_PATH)
+        sc_x = joblib.load(SCALER_X_PATH)
+        sc_y = joblib.load(SCALER_Y_PATH)
+
+        x = numpy.array([open_plan, rooms, area, kitchen_area, living_area]).reshape(1, -1)
+        x = sc_x.transform(x)
+        result = model.predict(x)
+        result = sc_y.inverse_transform(result.reshape(1, -1))
+
+        return str(result[0][0])
+
+    else:
+        return "not a valid model"
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5444, host='0.0.0.0')
